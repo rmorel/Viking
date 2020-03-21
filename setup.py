3b6a0b169a6d5dc59eec210fd8362ddfbed4f0aa
Rodrigo Morelatto <rodrigo.geof@gmail.com>
2020-03-21 11:21:39 -0300

    Change project structure to match data science cookiecutter's template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scripts to process the Mobil Avo Viking Graben Line 12 using only open source software',
+    author='Rodrigo Morelatto',
+    license='',
+)
